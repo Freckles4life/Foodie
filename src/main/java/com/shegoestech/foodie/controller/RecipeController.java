6448b7a73124f9f474e66565bf955e60a9222f4e
Elina <elina.razena@gmail.com>
2021-08-11 15:24:22 +0300

    Improved links between pages

--- src/main/java/com/shegoestech/foodie/controller/RecipeController.java
+++ src/main/java/com/shegoestech/foodie/controller/RecipeController.java
@@ -1,60 +1,44 @@
 package com.shegoestech.foodie.controller;
 
 import com.shegoestech.foodie.models.ChooseIngredients;
-import com.shegoestech.foodie.models.Ingredient;
 import com.shegoestech.foodie.models.Recipe;
 import com.shegoestech.foodie.service.IngredientService;
 import com.shegoestech.foodie.service.RecipeService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.boot.Banner;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.ModelAndView;
 
 import javax.validation.Valid;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Random;
-import java.util.stream.Collectors;
+
 
 @RequiredArgsConstructor
 @Controller
 @RequestMapping("/recipes")
 public class RecipeController {
     private final RecipeService recipeService;
-    private final IngredientService ingredientService;
-    private final ChooseIngredients chooseIngredients;
-    private final FoodieController foodieController;
 
 
     @GetMapping("/recipe-ingredients")
-    public String showIngredients(Model model){
-        model.addAttribute("chooseIngredients", chooseIngredients);
-        List<Ingredient> ingredients = ingredientService.getAll();
-
-        model.addAttribute("ingredients", ingredients);
-
+    public String chooseIngredientsForRecipe(Model model, Recipe recipe){
         return "recipe-ingredients";
     }
 
-
     @GetMapping("/add-recipe")
-    public String addRecipeInstructions(Model map) {
-        map.addAttribute("pageName", "Add Recipe Instructions");
-        map.addAttribute("recipe", new Recipe());
+    public String addRecipeInstructions(Model model, Recipe recipe) {
+        model.addAttribute("recipe", new Recipe());
         return "add-recipe";
     }
 
     @GetMapping("/success")
-    public String success(Model map) {
-        map.addAttribute("pageName", "You succeeded!");
+    public String success(Model model, Recipe recipe) {
         return "success";
     }
 
 
-    @PostMapping
+
+    @PostMapping("/add-recipe")
     public String saveRecipe(@Valid Recipe recipe, BindingResult result, Model model) {
         if (result.hasErrors()) {
             return "add-recipe";
@@ -62,6 +46,19 @@ public class RecipeController {
         recipeService.register(recipe);
         return "success";
     }
+
+    @PostMapping("/success")
+    public String saveRecipe2(@Valid Recipe recipe, BindingResult result, Model model) {
+         return "success";
+    }
+
+
+    // will save all chosen ingredients in the list
+//    @PostMapping("/recipe-ingredients")
+//    public String saveIngredientsForRecipe(Model model) {
+//        return "recipe-ingredients";
+//    }
+
 
 //    @GetMapping("/menu")
 //    public ModelAndView showMenu(ChooseIngredients  chooseIngredients){
