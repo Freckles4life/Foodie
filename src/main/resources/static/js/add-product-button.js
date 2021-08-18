a3de9a64ba74434dec13f7ddc4bde898754a5899
Elina <elina.razena@gmail.com>
2021-08-18 22:18:22 +0200

    Improvements with add-recipe js

--- src/main/resources/static/js/add-product-button.js
+++ src/main/resources/static/js/add-product-button.js
@@ -1,12 +1,13 @@
 $(document).ready(function(){
 
-var html = '<tr><td class="col-sm-4"><input type="text" class="form-control" placeholder="Product" aria-label="Product" name="ingredient[]" required list="ingredients" autocomplete="off" id="ingredientInput"/>' +
+var html = '<tr><td class="col-sm-4"><input type="text" class="form-control product" placeholder="Product" aria-label="Product" name="ingredient[]" required list="ingredients" autocomplete="off"/>' +
     '<datalist id="ingredients"><option th:each="i : ${ingredients}" th:text="${i.ingredientName}"></option></datalist></td>' +
     '<td class="col-sm-2"><input type="number" class="form-control" placeholder="Amount" name="amount[]" aria-label="Amount" required></td><td class="col-sm-2">' +
-    '<input type="text" class="form-control measure" placeholder="Measure" aria-label="Measure" id="units" disabled></td>' +
+    '<input type="text" class="form-control measure" placeholder="Measure" aria-label="Measure" disabled></td>' +
     '<td class="col-sm-2"><button type="button" class="btn btn-danger" name="remove" id="remove" value="remove">Remove</button></td></tr>'
 
 
+var i = 1;
 
 
       $("#input_field").on('click', '#remove', function(){
@@ -14,25 +15,33 @@ var html = '<tr><td class="col-sm-4"><input type="text" class="form-control" pla
         });
 
 
-      $("#add").on('click', function(){
-      i++;
-     $("#input_field").append(html);
-        });
-
-
-        $('tbody').on('change', '#ingredientInput', function(){
-          var x = document.getElementById("ingredientInput").value;
-//          document.getElementById("units").value = "gr";
-          $('#units').attr('placeholder', x);
-
+        $('form').on('click', '#add', function(){
+        i++;
+           $("#input_field").append(html);
+           $(".product").last().attr("id", "ingredientInput" + i);
+                $(".measure").last().attr("id", "units" + i);
         })
 
 
-//
-//document.getElementById("ingredientInput").addEventListener("change", function(){
-//  var x = document.getElementById("ingredientInput").value;
-//  document.getElementById("units").value = x;
-//});
+// code for insering ml, gr, pieces in fields
+
+        $('tbody').on('change', '#ingredientInput1', function(){
+        // x should be changed to respective unit for the product
+          var x = document.getElementById("ingredientInput1").value;
+              $('#units1').val(x);
+        })
+
+        $('tbody').on('change', '#ingredientInput2', function(){
+          var x = document.getElementById("ingredientInput2").value;
+              $('#units2').val(x);
+        })
+
+        $('tbody').on('change', '#ingredientInput3', function(){
+          var x = document.getElementById("ingredientInput3").value;
+              $('#units3').val(x);
+        })
+
+
 
 
 
